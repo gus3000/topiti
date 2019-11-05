107e423fcdef49b51e2497bb12c158f920355052
gus3000 <gus3000spam@gmail.com>
2019-11-05 03:26:14 +0100

    Topiti en session

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
-
+const session = require('express-session');
 
 const app = express();
 
-const films = ["Pursuit of Happyness", "I am Legend", "I, Robot"];
+app.use(session({
+    secret: "Will Smith",
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use((req,res,next) => {
+    if(!req.session.films)
+    {
+        req.session.films = ["Pursuit of Happyness", "I am Legend", "I, Robot"];
+    }
+    next();
+});
 
 app.use("/top", (req,res) => {
-    res.render("top.ejs", {films: films});
+    res.render("top.ejs", {films: req.session.films});
 });
 
 app.listen(8080);
